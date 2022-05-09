2f5107373b6ce7a71ded6df88180df4ffa3875c9
Alexan <alexanlima1.0@gmail.com>
2022-05-09 17:58:31 -0300

    Criando conexão com o banco de dados

---
+++ src/app/Program.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Connection connection = DB.getConnection();
+        DB.closeConnection();
+    }
+}
