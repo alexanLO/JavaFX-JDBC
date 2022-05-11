3c947963599e82271e322c77b8aabc8a9ef20f83
Alexan <alexanlima1.0@gmail.com>
2022-05-11 12:24:34 -0300

    Corrigindo o codigo do DepartmentFormController.java e DepartmentForm.fxml

--- src/controller/DepartmentFormController.java
+++ src/controller/DepartmentFormController.java
@@ -10,63 +10,36 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-<<<<<<< HEAD
 public class DepartmentFormController implements Initializable{
-    
-=======
-public class DepartmentFormController implements Initializable {
 
->>>>>>> 039ab388bab8fb0324e926283ebe89929962116d
     @FXML
     private TextField txtID;
     @FXML
     private TextField txtName;
     @FXML
-<<<<<<< HEAD
     private Button buttonSave;
     @FXML
     private Button buttonCancel;
     @FXML
-    private Label labelErrorName;
-  
-=======
-    private Label labelErrorName;
-    @FXML
-    private Button buttonSave;
-    @FXML
-    private Button buttonCancel;
+    private Label labelError;
 
->>>>>>> 039ab388bab8fb0324e926283ebe89929962116d
     @FXML
     public void onButtonSaveAction(){
-        System.out.println("Save!");
+        System.out.println("save");
     }
     @FXML
     public void onButtonCancelAction(){
-        System.out.println("Cancel!");
+        System.out.println("cancel");
     }
 
-<<<<<<< HEAD
-    public void initialize(URL location, ResourceBundle resources) {
-       InitializableNodes();
-        
-    }
-    
-    private void InitializableNodes() {
-=======
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        initializeNodes();
+    public void initialize(URL location, ResourceBundle resources) {
+     initializableNodes();
     }
 
-    private void initializeNodes(){
->>>>>>> 039ab388bab8fb0324e926283ebe89929962116d
+    private void initializableNodes() {
         Constraints.setTextFieldInteger(txtID);
         Constraints.setTextFieldMaxLength(txtName, 30);
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 039ab388bab8fb0324e926283ebe89929962116d
 }
