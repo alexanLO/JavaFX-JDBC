ba4cae797bdef60be8443cf1929b746c4cdcb54d
Alexan <alexanlima1.0@gmail.com>
2022-05-12 17:02:40 -0300

    criando as informações do formulario de vendendor

--- src/controller/SellerFormController.java
+++ src/controller/SellerFormController.java
@@ -1,8 +1,13 @@
 package Controller;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -19,6 +24,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -35,11 +41,23 @@ public class SellerFormController implements Initializable {
     @FXML
     private TextField txtName;
     @FXML
+    private TextField txtEmail;
+    @FXML
+    private DatePicker dpBirthDate;
+    @FXML
+    private TextField txtBaseSalary;
+    @FXML
     private Button buttonSave;
     @FXML
     private Button buttonCancel;
     @FXML
-    private Label labelError;
+    private Label labelErrorName;
+    @FXML
+    private Label labelErrorEmail;
+    @FXML
+    private Label labelErrorBirthDate;
+    @FXML
+    private Label labelErrorBaseSalary;
 
     public void setSeller(Seller entity) {
         this.entity = entity;
@@ -49,27 +67,26 @@ public class SellerFormController implements Initializable {
         this.service = service;
     }
 
-    public void subscribeDataChangeListener(DataChangeListener listener){
+    public void subscribeDataChangeListener(DataChangeListener listener) {
         dataChangeListeners.add(listener);
     }
 
     @FXML
     public void onButtonSaveAction(ActionEvent event) {
-        if(entity == null){
+        if (entity == null) {
             throw new IllegalStateException("Entity was null");
         }
-        if(service == null){
+        if (service == null) {
             throw new IllegalStateException("Service was null");
         }
-        try {     
+        try {
             entity = getFormData();
             service.saveOrUpdate(entity);
             notifyDataChangeListeners();
             Utils.currentStage(event).close();
-        } catch(ValidationException e){
+        } catch (ValidationException e) {
             setErrorMenssages(e.getErrors());
-        }
-         catch (Exception e) {
+        } catch (Exception e) {
             Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
         }
     }
@@ -87,11 +104,11 @@ public class SellerFormController implements Initializable {
 
         obj.setId(Utils.tryParseToInt(txtID.getText()));
 
-        if(txtName.getText() == null || txtName.getText().trim().equals("")){
+        if (txtName.getText() == null || txtName.getText().trim().equals("")) {
             exception.addError("Name", "Field can't be empty");
         }
         obj.setName(txtName.getText());
-        if(exception.getErrors().size() > 0){
+        if (exception.getErrors().size() > 0) {
             throw exception;
         }
 
@@ -110,7 +127,10 @@ public class SellerFormController implements Initializable {
 
     private void initializableNodes() {
         Constraints.setTextFieldInteger(txtID);
-        Constraints.setTextFieldMaxLength(txtName, 30);
+        Constraints.setTextFieldMaxLength(txtName, 70);
+        Constraints.setTextFieldDouble(txtBaseSalary);
+        Constraints.setTextFieldMaxLength(txtEmail, 60);
+        Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
     }
 
     public void updateFormData() {
@@ -119,13 +139,20 @@ public class SellerFormController implements Initializable {
         }
         txtID.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
+        txtEmail.setText(entity.getEmail());
+        Locale.setDefault(Locale.US);
+        txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+        if(entity.getBirthDate() != null){
+            dpBirthDate.setValue(LocalDateTime.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()).toLocalDate());
+        }
+
     }
 
-    private void setErrorMenssages(Map<String, String> errors){
+    private void setErrorMenssages(Map<String, String> errors) {
         Set<String> fields = errors.keySet();
 
-        if(fields.contains("Name")){
-            labelError.setText(errors.get("Name"));
+        if (fields.contains("Name")) {
+            labelErrorName.setText(errors.get("Name"));
         }
     }
 
--- src/controller/util/Utils.java
+++ src/controller/util/Utils.java
@@ -1,14 +1,18 @@
 package Controller.Util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 public class Utils {
 
@@ -25,6 +29,7 @@ public class Utils {
         }
     }
 
+    //References: https://stackoverflow.com/questions/47484280/format-of-date-in-the-javafx-tableview
     public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
         tableColumn.setCellFactory(column -> {
             TableCell<T, Date> cell = new TableCell<T, Date>() {
@@ -59,6 +64,34 @@ public class Utils {
                 }
             };
             return cell;
+        });
+    }
+
+    //References: https://stackoverflow.com/questions/26831978/javafx-datepicker-getvalue-in-a-specific-format
+    public static void formatDatePicker(DatePicker datePicker, String format) {
+        datePicker.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
+            {
+                datePicker.setPromptText(format.toLowerCase());
+            }
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
         });
     }
 }
